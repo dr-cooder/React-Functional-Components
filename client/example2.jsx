f511ba565def9b52d2fe5746ea11c5edc30e2142
Felix Warner (RIT Student) <fgw7468@rit.edu>
2022-10-26 13:50:26 -0400

    In-class exercises

---
+++ client/example2.jsx
@@ -0,0 +1,24 @@
+const HelloUser = (props) => {
+    const handleNameChange = (e) => {
+        ReactDOM.render(
+            <HelloUser username={e.target.value}/>,
+            document.getElementById('app')
+        )
+    };
+    return (
+        <div>
+            <p>Hello {props.username}!</p>
+            <label>Change Name:</label>
+            <input type="text" value={props.username}
+                onChange={handleNameChange}/>
+        </div>
+    );
+};
+
+const init = () => {
+    ReactDOM.render(
+        <HelloUser username='Gordon'/>,
+        document.getElementById('app')
+    )
+}
+window.onload = init;
